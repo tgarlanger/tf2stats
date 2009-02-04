6739443aa0ff668a3c20755a337b4a0129983812
tgarlanger <tgarlanger@dedda696-f2c5-11dd-b556-9305e745af9e>
2009-02-04 14:48:28 +0000

    Update code on SVN

--- tf2stats/tf2stats/Program.cs
+++ tf2stats/tf2stats/Program.cs
@@ -7,8 +7,28 @@ namespace tf2stats
 {
     class Program
     {
+        static void Error(int errnum)
+        {
+            string msg = "";
+
+            switch (errnum)
+            {
+                case 001:
+                    msg = "Error 001:  Too Few Arguments!";
+                    break;
+            }
+
+            Console.WriteLine(msg);
+        }
+
         static void Main(string[] args)
         {
+            if (args.Length <= 1)
+            {
+                Error(001);
+
+                return;
+            }
         }
     }
 }
