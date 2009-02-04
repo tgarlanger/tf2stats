06fb3e00817c35079caf61eae3df00e0d1aa8b92
tgarlanger <tgarlanger@dedda696-f2c5-11dd-b556-9305e745af9e>
2009-02-04 15:56:24 +0000

    Update the Code as well as commit the new Objects Namespace

---
+++ trunk/tf2stats/tf2stats/Objects/Objects.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tf2stats.Objects
+{
+    /// <summary>
+    /// User Class
+    /// </summary>
+    class User
+    {
+        /// <summary>
+        /// User Name of the Player
+        /// </summary>
+        private string username;
+        /// <summary>
+        /// Gets or Sets the User Name of the Player
+        /// </summary>
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        /// <summary>
+        /// Steam ID of the Player
+        /// </summary>
+        private string steamid;
+        /// <summary>
+        /// Gets or Sets the Steam ID of the Player
+        /// </summary>
+        public string SteamID
+        {
+            get
+            {
+                return steamid;
+            }
+            set
+            {
+                steamid = value;
+            }
+        }
+
+        /// <summary>
+        /// Team the Player is Currently Assigned to
+        /// </summary>
+        private string team;
+        /// <summary>
+        /// Gets or Sets the Team of the Player
+        /// </summary>
+        public string Team
+        {
+            get
+            {
+                return team;
+            }
+            set
+            {
+                team = value;
+            }
+        }
+
+        /// <summary>
+        /// Number of the Player
+        /// </summary>
+        private int number;
+        /// <summary>
+        /// Gets or Sets the Player Number
+        /// </summary>
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+    }
+}
--- trunk/tf2stats/tf2stats/Program.cs
+++ trunk/tf2stats/tf2stats/Program.cs
@@ -1,24 +1,48 @@
-﻿using System;
+﻿/// C# namespaces
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+/// tf2stats namespaces
+using tf2stats.Objects;
 
 namespace tf2stats
 {
     class Program
     {
+        /// <summary>  
+        /// Gets whether the specified path is a valid absolute file path.  
+        /// </summary>  
+        /// <param name="path">Any path. OK if null or empty.</param>  
+        static public bool IsValidPath(string path)
+        {
+            Regex r = new Regex(@"^(([a-zA-Z]\:)|(\\))(\\{1}|((\\{1})[^\\]([^/:*?<>""|]*))+)$");
+            return r.IsMatch(path);
+        }
+
+        /// <summary>
+        /// Accepts error code and displays appropriate message.
+        /// </summary>
+        /// <param name="errnum">Number of error to display.</param>
         static void Error(int errnum)
         {
-            string msg = "";
+            string msg = "Error " + errnum.ToString() + ":  ";
 
             switch (errnum)
             {
                 case 001:
-                    msg = "Error 001:  Too Few Arguments!";
+                    msg += "Too Few Arguments!";
+                    break;
+                case 002:
+                    msg += "Invalid File Path!";
                     break;
             }
 
-            Console.WriteLine(msg);
+            Debug.Fail(msg, errnum.ToString());
         }
 
         static void Main(string[] args)
@@ -28,6 +52,58 @@ namespace tf2stats
                 Error(001);
 
                 return;
+            }
+
+            StreamReader sr;
+            StreamWriter sw = new StreamWriter("output.txt");
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-i":
+                        if (i + 1 == args.Length)
+                        {
+                            Error(001);
+                        }
+                        if (!IsValidPath(args[i + 1]))
+                        {
+                            Error(002);
+                        }
+
+                        try
+                        {
+                            sr = new StreamReader(args[i + 1]);
+                        }
+                        catch (Exception e)
+                        {
+                            
+                        }
+
+                        i++;
+                        break;
+                    case "-o":
+                        if (i + 1 == args.Length)
+                        {
+                            Error(001);
+                        }
+                        if (!IsValidPath(args[i + 1]))
+                        {
+                            Error(002);
+                        }
+
+                        try
+                        {
+                            sw = new StreamWriter(args[i + 1]);
+                        }
+                        catch (Exception e)
+                        {
+
+                        }
+
+                        i++;
+                        break;
+                }
             }
         }
     }
